d9b1ef24f2c005878e93f2d5e9a07b479d05fe99
Sorokin Ivan <Spanhbob98@mail.ru>
2024-07-06 18:12:36 +0300

    доработка формы js

--- app/js/form_aut.js
+++ app/js/form_aut.js
@@ -1,17 +1,39 @@
-let ent = document.getElementById("entrance");
+//Появление формы авторизации
+let aut = document.getElementById("auth");
 
-ent.addEventListener("click", function (event) {
-    let div = event.target.closest('#entrance');
+aut.addEventListener("click", function (event) {
+    let div = event.target.closest('#auth');
     if (!div) return;
     let form = document.getElementById("aut");
     form.classList.toggle('show');
 });
 
-let closed = document.getElementById("close");
+//Закрытие формы авторизации
+let autClosed = document.getElementById("aut-close");
 
-closed.addEventListener("click", function (event) {
+autClosed.addEventListener("click", function (event) {
     let div = event.target.closest('#aut');
     if (!div) return;
     let form = document.getElementById("aut");
+    form.classList.toggle('show');
+});
+
+//Появление формы регистрации
+let reg = document.getElementById("reg");
+
+reg.addEventListener("click", function (event) {
+    let div = event.target.closest('#reg');
+    if (!div) return;
+    let form = document.getElementById("form-reg");
+    form.classList.toggle('show');
+});
+
+//Закрытие формы регистрации
+let regClosed = document.getElementById("reg-close");
+
+regClosed.addEventListener("click", function (event) {
+    let div = event.target.closest('#form-reg');
+    if (!div) return;
+    let form = document.getElementById("form-reg");
     form.classList.toggle('show');
 });
