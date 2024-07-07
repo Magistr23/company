e8c466df6385cc58fefbdb5652fd25162b57dd4d
Sorokin Ivan <Spanhbob98@mail.ru>
2024-07-07 22:33:01 +0300

    изменение класса подключения

--- app/controllers/connect.php
+++ app/controllers/connect.php
@@ -4,23 +4,20 @@ namespace App\Controllers;
 use PDO;
 use PDOException;
 
-class Connect {
+class connect {
     private $host = 'localhost';
     private $bd_name = 'company';
     private $username = 'root';
     private $password = 'root';
     public $conn;
 
-    public function getConnection()
+    public function __construct()
     {
 
         try {
             $this->conn = new PDO("mysql:host=".$this->host.";dbname=".$this->bd_name.";charset=utf8" , $this->username, $this->password);
-            $this->conn->exec("set names utf8");
         }catch (PDOException $exception) {
             echo "Connection error: " . $exception->getMessage();
         }
-
-        return $this->conn;
     }
 }
